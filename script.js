38aee917c4a5497ea26a1ae05183221a58f2c94d
prashantkusahis <kumarprashantsahis@gmail.com>
2023-12-08 22:40:05 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use Strict";
+
+//Function to calculate age
+const ageCalculate = () => {
+  //get today's date
+  const today = new Date();
+
+  //user input birth date
+  const inputDate = new Date(document.getElementById("date-input").value);
+
+  const birthTime = inputDate.getTime();
+  const currentTime = today.getTime();
+
+  if (birthTime > currentTime) {
+    alert("Not Born Yet");
+    displayResult("-", "-", "-");
+    return;
+  }
+
+  const diffTime = currentTime - birthTime;
+
+  const years = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365));
+
+  const months = Math.floor(
+    (diffTime % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30.44)
+  );
+
+  const days = Math.floor(
+    (diffTime % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
+  );
+
+  displayResult(days, months, years);
+};
+
+const displayResult = (days, months, years) => {
+  document.getElementById("years").textContent = years;
+  document.getElementById("months").textContent = months;
+  document.getElementById("days").textContent = days;
+};
+
+// Event listener for the "Enter" key
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    ageCalculate();
+  }
+});
+
+document.getElementById("calc-age-btn").addEventListener("click", ageCalculate);
